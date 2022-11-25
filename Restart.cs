b0bdfad447458e7252605b200e20e4973fa875c6
Neilin <90468765+Neilin@users.noreply.github.com>
2022-11-26 03:46:14 +0700

    Add files via upload

---
+++ Restart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+
+    public void StarGame()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+}
